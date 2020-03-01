c55d7c9ef29ed331705e7233cf2a13ae2709d694
Philipp Töws <ptoews@uni-koblenz.de>
2020-03-01 22:02:17 +0100

    Allow running lea via function arguments for server edition

--- lea.py
+++ lea.py
@@ -7,50 +7,59 @@ import klips_checker
 import lea_exceptions
 import telegram_notificator
 
+
 script_path = os.path.dirname(os.path.realpath(__file__))
 # TODO: Make configurable
 HISTORIES_PATH = os.path.join(script_path, "histories")
 
-if not os.path.exists(HISTORIES_PATH):
-    os.mkdir(HISTORIES_PATH)
 
-with open(os.path.join(script_path, "conf.json")) as fo:
-    conf = json.load(fo)
+def run(telegram_api_key, telegram_chat_id, klips_email, klips_password):
+    if not os.path.exists(HISTORIES_PATH):
+        os.mkdir(HISTORIES_PATH)
 
-email = conf["uni-email-address"]
+    checkers = [
+        klips_checker.KLIPSChecker(klips_email, klips_password, HISTORIES_PATH)
+    ]
 
-telegram_api_key = conf["telegram_api_key"]
-telegram_chat_id = conf["telegram_chat_id"]
+    notificators = [
+        telegram_notificator.TelegramNotificator(telegram_api_key, telegram_chat_id)
+    ]
 
-# Can this be read out when running? Maybe split the password into different variables so they don't lie in one tracable register? Or a reversible encryption technique?
-password = getpass.getpass("Enter your password: ")
+    timer = 0
+
+    while True:
+        for c in checkers:
+            if timer % c.get_interval_length() == 0:
+                try:
+                    diff_info = c.check()
+                    if diff_info is not None:
+                        print(diff_info)
+                        for n in notificators:
+                            n.send(str(diff_info))
+                    else:
+                        print("No change detected")
+                except lea_exceptions.CheckingFailedException as e:
+                    # TODO: Error handling
+                    print(e)
+        time.sleep(60)
+        timer += 1
 
 
-### For Testing
-checkers = [
-    klips_checker.KLIPSChecker(email, password, HISTORIES_PATH)
-]
+def run_from_config():
+    with open(os.path.join(script_path, "conf.json")) as fo:
+        conf = json.load(fo)
 
-notificators = [
-    telegram_notificator.TelegramNotificator(telegram_api_key, telegram_chat_id)
-]
+    klips_email = conf["uni-email-address"]
 
-timer = 0
+    telegram_api_key = conf["telegram_api_key"]
+    telegram_chat_id = conf["telegram_chat_id"]
 
-while True:
-    for c in checkers:
-        if timer % c.get_interval_length() == 0:
-            try:
-                diff_info = c.check()
-                if diff_info is not None:
-                    print(diff_info)
-                    for n in notificators:
-                        n.send(str(diff_info))
-                else:
-                    print("No change detected")
-            except lea_exceptions.CheckingFailedException as e:
-                # TODO: Error handling
-                print(e)
-    time.sleep(60)
-    timer += 1
+    # Can this be read out when running? Maybe split the password into different variables so they
+    # don't lie in one tracable register? Or a reversible encryption technique?
+    klips_password = getpass.getpass("Enter your password: ")
 
+    run(telegram_api_key, telegram_chat_id, klips_email, klips_password)
+
+
+if __name__ == "__main__":
+    run_from_config()
