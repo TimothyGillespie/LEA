3dbdd1566c9cb511f6fb48f7abf799fc8c3e017c
Philipp Töws <ptoews@uni-koblenz.de>
2020-03-01 18:12:43 +0100

    Implement prettier formatting for most frequent diff case

--- telegram_notificator.py
+++ telegram_notificator.py
@@ -2,6 +2,7 @@ import requests
 
 import notificator
 
+
 class TelegramNotificator(notificator.Notificator):
 	
 	def __init__(self, api_key: str, chat_id: str):
@@ -10,8 +11,14 @@ class TelegramNotificator(notificator.Notificator):
 		self.chat_id = chat_id
 		
 	def send(self, message: str) -> bool:
+		"""
+		Sends the given message to the configured chat_id via the configured telegram bot.
+		:param message: The message as string, formatted with markdown if desired.
+		:return: True if the sending was successful.
+		"""
 		payload = {
 			"chat_id": self.chat_id,
+			"parse_mode": "markdown",
 			"text": message
 		}
 		
